d2844c47744b609dabfb80ca0a6ac678d5301027
Jitendr Choudhary <Jitendra93266@gmail.com>
2018-08-09 15:15:53 +0530

    Simple Blockchain - creates new block and have previous hash

---
+++ simpleChain.js
@@ -0,0 +1,35 @@
+
+const SHA256 = require('crypto-js/sha256')
+/*
+Block Class is the class for a block 
+Basic unit of blockchain
+It have only one constructore
+*/
+class Block {
+    constructor(data) {
+      this.hash = "",
+        this.height = 0,
+        this.body = data,
+        this.time = 0,
+        this.previousBlockHash = ""
+    }
+  }
+/*
+Class for blockchain
+have 1 constructor
+*/
+
+class Blockchain{
+    constructor(){
+        this.chain=[]
+        this.addBlock(new Block("Genesis Block in chain"));
+    }
+    addBlock(newBlock){
+        if(this.chain.length>0){
+            newBlock.previousBlockHash=this.chain[this.chain.length-1].hash;
+        }
+        newBlock.hash=SHA256(JSON.stringify(newBlock)).toString();
+
+        this.chain.push(newBlock);
+    }
+}
